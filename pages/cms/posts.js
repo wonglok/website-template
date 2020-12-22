753c3983ed9143d119a734472c576bd54e3bd2df
wonglok <yellowhappy831@gmail.com>
2020-12-23 06:55:16 +0800

    git backup

--- pages/cms/posts.js
+++ pages/cms/posts.js
@@ -36,7 +36,10 @@ export default function CMSLandingPage () {
     <div>
       <Head>
         <title>86deck</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/fav/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/fav/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/fav/favicon-16x16.png" />
+        <link rel="icon" href="/fav/favicon.ico" />
       </Head>
       {canShow ? <CMSApp></CMSApp> : null}
     </div>
